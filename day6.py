ea0fd979700f0033adb364fd923456be2e53421e
agentscreech <travis.walentin@gmail.com>
2020-01-07 20:02:37 -0800

    day6 pt 1

---
+++ day6.py
@@ -0,0 +1,54 @@
+from helpers import *
+from collections import defaultdict, deque
+
+
+class Node():
+    def __init__(self, name):
+        self.parent = None
+        self.children = []
+        self.name = name
+        self.distance = None
+
+
+planets = defaultdict()
+
+data = get_input(6)
+
+#build tree
+for line in data:
+    orbit = line[:-1].split(")")
+    if orbit[0] not in planets:
+        planets[orbit[0]] = Node(orbit[0])
+    if orbit[1] not in planets:
+        planets[orbit[1]] = Node(orbit[1])
+    planets[orbit[0]].children.append(planets[orbit[1]])
+    if planets[orbit[1]].parent is None:
+        planets[orbit[1]].parent = planets[orbit[0]]
+
+def get_distance(node):
+    hops = 0
+    first = node
+    while node.parent:
+        node = node.parent
+        if node.distance:
+            first.distance = hops + node.distance + 1
+            return first.distance
+        hops += 1
+    first.distance = hops
+    return hops
+
+#BFS and count the number of hops from node to COM to get total indirect orbits
+counter = 0
+queue = deque([planets["COM"]])
+
+while queue:
+    planet = queue.pop()
+    if planet.children:
+        for child in planet.children:
+            queue.append(child)
+    counter += get_distance(planet)
+
+
+
+print(counter)
+
