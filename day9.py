8d20a83a6522774e7961e826489da27edfcaf980
Travis Walentin <travis.walentin@oracle.com>
2020-01-21 14:25:54 -0800

    Day 9 done

--- day9.py
+++ day9.py
@@ -1,13 +1,11 @@
 from helpers import intcode, get_input
 
-# t1 = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-# t2 = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
-# t3 = [104, 1125899906842624, 99]
-# opcodes = list(map(lambda x: str(x), t3))
 opcodes = []
 data = get_input(9)
 for line in data:
     opcodes = line.split(',')
+opcodes = list(map(lambda x: int(x), opcodes))
 
-o = intcode(opcodes, [1])
-print(o)
+p1 = intcode(opcodes, [1])
+p2 = intcode(opcodes, [2])
+print(p1[0], p2[0])
